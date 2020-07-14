dc675e1a40475081c7c8478d828eef9a2fb70665
Vinicius Marques <mViniciusMarques@users.noreply.github.com>
2020-07-14 19:55:25 -0300

    Update Boletim.java
    as

--- java/model/Boletim.java
+++ java/model/Boletim.java
@@ -24,10 +24,7 @@ import javax.persistence.TemporalType;
 import javax.persistence.UniqueConstraint;
 import javax.xml.bind.annotation.XmlRootElement;
 
-/**
- *
- * @author tiago
- */
+
 @Entity
 @Table(catalog = "zoo", schema = "", uniqueConstraints = {
     @UniqueConstraint(columnNames = {"id"})})
